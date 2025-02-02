b22921bdbc717b41a348ed90effb7083cd7c188a
rashid-mamun <rashid@sharetrip.net>
2025-02-02 10:46:25 +0600

    Fix: Implemented challenges

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import { ERROR_MESSAGES } from "../constants/messages";
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { ERROR_MESSAGES } from '../constants/messages';
 import { redisClient } from '../utils/redis';
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
@@ -13,25 +13,21 @@ export const authenticate = async (
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
-    return;
+    return res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
   }
 
   try {
     const isBlacklisted = await redisClient.get(`blacklist_${token}`);
     if (isBlacklisted) {
-      res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
-      return;
+      console.error('Token is blacklisted:', token);
+      return res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
     }
 
-
-    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET) as {
-      id: string;
-    };
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET) as { id: string };
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
-    return;
+    console.error('Token verification failed:', err);
+    return res.status(401).json({ error: ERROR_MESSAGES.UNAUTHORIZED });
   }
 };
